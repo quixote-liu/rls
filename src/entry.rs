417e8c71ee7c74396c5296ac6971b1b95f3eb9e0
quixote-lcs <lcs.shun@foxmail.com>
2024-08-08 23:33:04 +0800

    add render logic and entry code

--- src/entry.rs
+++ src/entry.rs
@@ -1,14 +1,58 @@
-use std::fs;
+use std::{fmt::Write, fs};
 
 pub struct Entry {
     dir_entry: fs::DirEntry,
+    front_entry: Box<Option<Entry>>,
+    next_entry: Box<Option<Entry>>,
+    file_name: String,
+
+    prefix: String,
+    content: String,
+    suffix: String,
+    // colour: String,
 }
 
 impl Entry {
-    pub fn from(e :fs::DirEntry) -> Self {
-        Self{
-            dir_entry: e,
+    pub fn new(dir_entry :fs::DirEntry, front_entry: Option<Entry>, next_entry: Option<Entry>) -> Self {
+        let mut prefix = String::new();
+        if front_entry.is_some() {
+            prefix = "\t".to_string();
         }
+        let mut suffix = String::new();
+        if next_entry.is_none() {
+            suffix = "\n".to_string();
+        }
+
+        let mut file_name = String::new();
+        if let Ok(v) = dir_entry.file_name().into_string() {
+            file_name = v;
+        }
+
+        Self{
+            dir_entry: dir_entry,
+            front_entry: Box::new(front_entry),
+            next_entry: Box::new(next_entry),
+            file_name,
+            prefix,
+            content: file_name,
+            suffix,
+        }
+    }
+
+    pub fn set_next_entry(&self, next_entry: Option<Entry>) {
+        self.next_entry = Box::new(next_entry);
+    }
+
+    pub fn set_front_entry(&self, front_entry: Option<Entry>) {
+        self.front_entry = Box::new(front_entry);
+    }
+
+    pub fn render(&self) -> String {
+        let mut r = String::new();
+        let _ = r.write_str(&self.prefix);
+        let _ = r.write_str(&self.content);
+        let _ = r.write_str(&self.suffix);
+        r
     }
 }
 
--- src/render.rs
+++ src/render.rs
@@ -1,93 +1,108 @@
+use std::collections::HashMap;
 use std::env;
+use std::fmt::Error;
 use std::fs;
 use std::path;
 use crate::entry::Entry;
 
 pub struct Render {
-    entrys: Box<Vec<Entry>>,
-    error: std::io::Error,
+    header_entry: Box<Option<Entry>>,
+}
+
+enum SubOption {
+    All,
+    Time,
+    Long,
 }
 
 impl Render {
-    pub fn new() -> Self {
+    pub fn init() -> Result<Self, std::io::Error> {
+        let mut sub_options = Vec::new();
+        let mut is_all_entry = false;
+        let mut all_chars = HashMap::new();
 
-
-        let entrys = Vec::from());
-        
-        Self{
-            entrys: Box::new(),
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct DirEntryFormat {
-    entrys: Vec<fs::DirEntry>,
-    entry_errs: Option<Vec<std::io::Error>>,
-    other_err: Option<std::io::Error>,
-}
-
-// enum SubOption {
-//     All,
-//     Time,
-//     Help,
-// }
-
-impl DirEntryFormat {
-    pub fn init() -> Self {
-        let mut dir_entry_format = DirEntryFormat::default();
-
-        let cur_dir_path: path::PathBuf;
-        match env::current_dir() {
-            Ok(path) => {cur_dir_path = path},
-            Err(e) => {
-                dir_entry_format.other_err = Some(e);
-                return dir_entry_format;
-            },
-        }
-
-        let mut entry_errs: Vec<std::io::Error> = Vec::new();
-        match fs::read_dir(cur_dir_path) {
-            Ok(dir) => {
-                for entry in dir {
-                    match entry {
-                        Ok(entry)=>{dir_entry_format.entrys.push(entry)},
-                        Err(e) =>{ entry_errs.push(e) },
-                    }
-                }
-            },
-            Err(e) => {
-                dir_entry_format.other_err = Some(e);
-                return dir_entry_format;
-            },
-        }
-        if entry_errs.len() > 0 {
-            dir_entry_format.entry_errs = Some(entry_errs);
-        }
-
-        dir_entry_format
-    }
-
-    pub fn output(&self) {
-        if let Some(err) = &self.other_err {
-            println!("{}", err);
-            return;
-        }
-
-        let mut res = String::new();
-        for e in &self.entrys {
-            let file_name = e.file_name();
-            let mut file_name_str = String::new();
-            if let Ok(v) = file_name.into_string() {
-                file_name_str = v;
-            }
-            if file_name_str.is_empty() || file_name_str.starts_with(".") {
+        let args: Vec<String> = env::args().collect();
+        for arg in args {
+            if !arg.starts_with("-") || arg.starts_with("--") {
                 continue;
             }
-            res.push_str(&file_name_str);
-            res.push('\t');
-        }
-
-        println!("{}", res);
+            let a = arg.trim_start_matches("-");
+            for c in a.chars() {
+                match c {
+                    "a" => {is_all_entry = true},
+                    "t" => { sub_options.push(SubOption::Time) },
+                    "l" => { sub_options.push(SubOption::Long) },
+                    _ => { 
+                        return Err()    
+                    },
+                }
+            }
+        };
     }
 }
+
+// #[derive(Default)]
+// pub struct DirEntryFormat {
+//     entrys: Vec<fs::DirEntry>,
+//     entry_errs: Option<Vec<std::io::Error>>,
+//     other_err: Option<std::io::Error>,
+// }
+
+// impl DirEntryFormat {
+//     pub fn init() -> Self {
+//         let mut dir_entry_format = DirEntryFormat::default();
+
+//         let cur_dir_path: path::PathBuf;
+//         match env::current_dir() {
+//             Ok(path) => {cur_dir_path = path},
+//             Err(e) => {
+//                 dir_entry_format.other_err = Some(e);
+//                 return dir_entry_format;
+//             },
+//         }
+
+//         let mut entry_errs: Vec<std::io::Error> = Vec::new();
+//         match fs::read_dir(cur_dir_path) {
+//             Ok(dir) => {
+//                 for entry in dir {
+//                     match entry {
+//                         Ok(entry)=>{dir_entry_format.entrys.push(entry)},
+//                         Err(e) =>{ entry_errs.push(e) },
+//                     }
+//                 }
+//             },
+//             Err(e) => {
+//                 dir_entry_format.other_err = Some(e);
+//                 return dir_entry_format;
+//             },
+//         }
+//         if entry_errs.len() > 0 {
+//             dir_entry_format.entry_errs = Some(entry_errs);
+//         }
+
+//         dir_entry_format
+//     }
+
+//     pub fn output(&self) {
+//         if let Some(err) = &self.other_err {
+//             println!("{}", err);
+//             return;
+//         }
+
+//         let mut res = String::new();
+//         for e in &self.entrys {
+//             let file_name = e.file_name();
+//             let mut file_name_str = String::new();
+//             if let Ok(v) = file_name.into_string() {
+//                 file_name_str = v;
+//             }
+//             if file_name_str.is_empty() || file_name_str.starts_with(".") {
+//                 continue;
+//             }
+//             res.push_str(&file_name_str);
+//             res.push('\t');
+//         }
+
+//         println!("{}", res);
+//     }
+// }
